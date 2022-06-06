75d1fe16ffacac8a2928587fc6d9831acb7b74e1
erichvvs <erich.adan@gmail.com>
2022-06-06 11:41:34 -0500

    javascript pagina y document.write

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 /*Mensaje de alerta de inicio*/
 
-alert ('Con toda la humildad, no sienta que wuau cuando miuau!!');
+alert ('Con toda la humildad, no sienta que wuau cuando miau!!');
+
+/*Datos de nombre
+
+var nombre = "Erich Vivas";
+var altura = "172";
+
+document.write (nombre);
+document.write (altura);*/
+
+/*Datos por agrupación*/
+
+var nombre = "Erich Vivas";
+var altura = "172";
+
+var concatenacion = nombre + " " + altura
+
+document.write (concatenacion);
+
+
