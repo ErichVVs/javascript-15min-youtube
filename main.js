8891b83a2ffe1cfa6a43a32e29bab034dca772fe
erichvvs <erich.adan@gmail.com>
2022-06-06 11:56:30 -0500

    datos por innerHTML

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*Mensaje de alerta de inicio*/
+/*Mensaje de alerta de inicio
 
 alert ('Con toda la humildad, no sienta que wuau cuando miau!!');
 
@@ -10,13 +10,41 @@ var altura = "172";
 document.write (nombre);
 document.write (altura);*/
 
-/*Datos por agrupación*/
+
+
+/*Datos por agrupación
 
 var nombre = "Erich Vivas";
 var altura = "172";
 
 var concatenacion = nombre + " " + altura
 
-document.write (concatenacion);
+document.write (concatenacion);*/
 
 
+
+/*Datos por getElement
+
+var nombre = "Erich Vivas";
+var altura = 172;
+
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById ("datos");
+datos.innerHTML = concatenacion;
+
+
+/*Datos por Templets String*/
+
+var nombre = "Erich Vivas";
+var altura = 172;
+
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById ("datos");
+
+datos.innerHTML = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Mi altura es: ${altura} cm</h3>
+`;
